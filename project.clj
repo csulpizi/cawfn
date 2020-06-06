fbb0369e3c88e5d3f8bfa7931c460e3bc3be537a
Cory Sulpizi <cory.sulpizi@gmail.com>
2020-06-06 17:40:15 -0400

    .

--- project.clj
+++ project.clj
@@ -2,4 +2,8 @@
   :description "sdefn: a compile-time aware alternative to defn"
   :url "https://github.com/csulpizi/sdefn"
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns sdefn.core})
+  :repl-options {:init-ns sdefn.core}
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "Clojure license."})
